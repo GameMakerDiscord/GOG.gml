c5ae0219cd0f872c8e8b8ac4f32de2d3c39bd281
YellowAfterlife <yellowafterlife@ymail.com>
2018-01-17 10:46:12 +0200

    Link up Mac version in the extension, auto-copy GOG binary after compile.

--- build-osx.sh
+++ build-osx.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 g++ -m32 -O3 -fPIC -g -c -o GOG.gml/GOG.gml.o GOG.gml/GOG.gml.cpp -std=c++11 
 g++ -m32 GOG.gml/GOG.gml.o -shared -o GOG.gml.gmx/extensions/GOG.gml/GOG.gml.dylib -Wl,-rpath,assets/ -LGOG/Libraries -lgalaxy
+cp GOG/Libraries/libGalaxy.dylib GOG.gml.gmx/extensions/GOG.gml/libGalaxy.dylib
